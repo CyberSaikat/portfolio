f1e5ddbebdf927c4dfc3f655d5961780c56b8ba7
Saikat Roy <saikatroydot@gmail.com>
2023-09-29 02:09:26 +0530

    almost

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,31 @@
-$(document).ready(function(){
-    $(".controller i").click(function(){
-        $('.controller').toggleClass("active");
-        $(".menu").toggleClass("active");
-    });
-    $("#experience").html(new Date().getFullYear() - 2020);
+$(document).ready(function () {
+  $(".controller i").click(function () {
+    $(".controller").toggleClass("active");
+    $(".menu").toggleClass("active");
+  });
+  $("#experience").html(new Date().getFullYear() - 2020);
+  const screensize = screen.width;
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    breakpoints: {
+      860: {
+        slidesPerView: 2,
+      },
+    },
+    spaceBetween: 30,
+    direction: "horizontal",
+    loop: true,
+    autoplay: true,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
